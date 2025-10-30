680f638f055e83b672eac6d9bc93a65ce6aa438c
grizz <grizzlyenot@gmail.com>
2025-10-30 17:20:34 +0300

    Added main menu colors, button captions and YS font family

--- app/src/main/java/com/practicum/playlist_maker_android_vladimirovaleksei/ui/theme/Type.kt
+++ app/src/main/java/com/practicum/playlist_maker_android_vladimirovaleksei/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.practicum.playlist_maker_android_vladimirovaleksei.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.practicum.playlist_maker_android_vladimirovaleksei.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,8 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val yandexSansFamily = FontFamily(
+    Font(R.font.ys_medium, FontWeight.Medium)
 )
