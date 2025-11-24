f16289ff76462b0b07bee6971e3e5aac0bde8e2c
grizz <grizzlyenot@gmail.com>
2025-11-24 22:27:22 +0300

    Imports optimization

--- app/src/main/java/com/practicum/playlist_maker_android_vladimirovaleksei/MainActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker_android_vladimirovaleksei/MainActivity.kt
@@ -19,10 +19,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,8 +26,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.practicum.playlist_maker_android_vladimirovaleksei.ui.theme.YpDarkGray
 import com.practicum.playlist_maker_android_vladimirovaleksei.ui.theme.MainBlue
+import com.practicum.playlist_maker_android_vladimirovaleksei.ui.theme.YpDarkGray
 import com.practicum.playlist_maker_android_vladimirovaleksei.ui.theme.yandexSansMedium
 
 class MainActivity : ComponentActivity() {
